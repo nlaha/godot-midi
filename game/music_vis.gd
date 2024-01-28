d297437ed7dd07a97ad09b77478d1dd2f8e87478
Nathan <nlaha@outlook.com>
2024-01-28 11:37:52 -0800

    add manual ASP sync to demo

--- game/music_vis.gd
+++ game/music_vis.gd
@@ -6,15 +6,35 @@ var notes = []
 var notes_on = {}
 
 var midi_player: MidiPlayer
+var asp: AudioStreamPlayer
+
+@onready var last_time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	midi_player = $MidiPlayer
+	asp = $AudioStreamPlayer
+	# we set the following to true so we can tick the midi player from
+	# within our script, this is necessary for accurate syncing with the asp
+	midi_player.manual_process = true
 	midi_player.note.connect(on_note)
 	midi_player.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+
+	# get asp playback time
+	var time = asp.get_playback_position() + AudioServer.get_time_since_last_mix()
+	# Compensate for output latency.
+	time -= AudioServer.get_output_latency()
+	
+	# tick the midi player with the delta from our audio stream player
+	# this syncs the midi player with the audio server
+	# this is a more accurate way of doing it than using the delta from _process
+	var asp_delta = time - last_time
+	last_time = time
+	midi_player.process_delta(asp_delta)
+
 	# spawn notes
 	for note in notes_on:
 		# spawn a cube
